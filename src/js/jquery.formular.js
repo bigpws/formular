1a331a666327dd26621a770f02a3540f0b88bd84
sun.chen <sun.chen@pengjinsoft.com>
2017-05-05 17:11:32 +0800

    第一天

--- src/js/jquery.formular.js
+++ src/js/jquery.formular.js
@@ -17,45 +17,86 @@
         factory(jQuery);
     }
 }(this, function ($) {
-    var key_code;
     var Formular = function(element, options) {
-        this.element = element,
-            this.defaults = {
-                style:{
-                    'color': 'red',
-                    'fontSize': '12px',
-                    'textDecoration':'none',
-                },
-                mathematical_functions:[
-                    'sin','cos','tan','log'
-                ],
-                operators:[
-                    '+','-','*','/','%'
-                ],
-                vars_flag:'$',
+        this.element = $(element),
+        this.defaults = {
+            style:{
+                'color': 'red',
+                'fontSize': '12px',
+                'textDecoration':'none',
             },
-            this.options = $.extend({}, this.defaults, options)
+            mathematical_functions:[
+                'sin','cos','tan','log'
+            ],
+            operators:[
+                '+','-','*','/','%'
+            ],
+            varsFlag:'$',
+        },
+        this.listStatus = false;
+        this.options = $.extend({}, this.defaults, options);
     };
     Formular.prototype = {
-        init: function() {
+        keyCode: {
+            BACKSPACE: 8,
+            COMMA: 188,
+            DELETE: 46,
+            DOWN: 40,
+            END: 35,
+            ENTER: 13,
+            ESCAPE: 27,
+            HOME: 36,
+            LEFT: 37,
+            PAGE_DOWN: 34,
+            PAGE_UP: 33,
+            PERIOD: 190,
+            RIGHT: 39,
+            SPACE: 32,
+            TAB: 9,
+            UP: 38
+        },
+        _create: function() {
             this.element.css({
                 'color': this.options.style.color,
                 'fontSize': this.options.style.fontSize,
                 'textDecoration': this.options.style.textDecoration
             });
-            this.element.keyup=this.onKeyUp(e);
+            var _resultSelector = '<ul class=\'formular_result\'>';
+            $.each(this.options.accessVars,function(index,value){
+                _resultSelector = _resultSelector+'<li>'+value+'('+index+')'+'</li>';
+            });
+            _resultSelector = _resultSelector+'</ul>';
+            this.element.after(_resultSelector);
+            $('.formular_result li').hover(function(){
+                $(this).addClass('blue');
+            },function () {
+                $(this).removeClass('blue');
+            });
+            this.element.bind('keyup',this.options,function(event){
+                var options = event.data;
+                $(options.explain_panel).html(event.target.value);
+                if (event.keyCode ===52 &&event.key===options.varsFlag){
+                    $('.formular_explain').html(event.currentTarget.html);
+                    $('.formular_result').css('display','block');
+                }
+                //console.log(this.options,event)
+            });
+            // this.element.keydown(function(event){
+            //     if (event.key==this.options.varsFlag){
+            //         console.log('ok');
+            //     }
+            //     // console.log(event.target);
+            // });
             return Formular;
         },
-        onKeyUp:function(e){
-            if (e.target.value==='$'){
-                console.log('catch it');
-            }
-            return Formular;
-        }
+        // _registerKeyDownHandles:function(event){
+        //     // var target = event.target;
+        //     console.log(event);
+        // },
     }
     $.fn.formular = function (options) {
         var formular = new Formular(this,options);
-        return formular.init();
+        return formular._create();
     };
 
 }));
